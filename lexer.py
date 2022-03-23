7c3a151d1457a16f4889bee431851f267ac8dcf2
Gregory Holder <gregory.holder76@gmail.com>
2022-03-23 11:53:46 +0100

    added newline counting

--- lexer.py
+++ lexer.py
@@ -40,6 +40,11 @@ def t_ID(t):
     t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
 
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
 # Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])

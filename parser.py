dfeca9a5ca8dff8ae8bf2a98b8bed4b00d88189f
Gregory Holder <gregory.holder76@gmail.com>
2022-03-24 09:29:47 +0100

    Parser now uses tuple nodes

--- parser.py
+++ parser.py
@@ -1,48 +1,45 @@
 from collections import namedtuple
-from logging.config import IDENTIFIER
 import ply.yacc as yacc
 from lexer import tokens
 
 
-VarDefs = namedtuple("Variables", ["var_line_defs"])
 def p_var_defs(p):
     ''' var_defs : VARIABLES ":" NEWLINE
                   | var_defs var_line_def'''
     if len(p) == 4:
-        p[0] = VarDefs([])
+        p[0] = ("var_defs", [])
     elif p[2]:
-        p[0] = VarDefs(p[1].var_line_defs + [p[2]])
+        p[0] = ("var_defs", p[1][1] + [p[2]])
     else:
-        p[0] = VarDefs(p[1].var_line_defs)
+        p[0] = ("var_defs", p[1][1])
 
 
-VarLineDef = namedtuple("VarLineDef", ["vars", "type"])
 def p_var_line_def(p):
     '''var_line_def : id_list ':' type NEWLINE
                     | NEWLINE'''
     if len(p) == 5:
-        p[0] = VarLineDef(p[1], p[3])
+        p[0] = ("var_line_def", p[1], p[3])
     else:
-        p[0] = None
+        p[0] = ("var_line_def")
 
-Type = namedtuple("Type", ["type", "is_pointer"])
 def p_type(p):
-    '''type : ID
+    '''type : id
             | ptr_vers type'''
     if len(p) == 3:
-        p[0] = Type(p[2], True)
+        p[0] = ("ptr_type", p[2])
     else:
-        p[0] = Type(p[1], False)
+        p[0] = ("type", p[1])
+        
 
 # A list of IDs seperated by an optional coma
 def p_id_list(p):
-    '''id_list : ID opt_coma
-               | id_list ID opt_coma
+    '''id_list : id opt_coma
+               | id_list id opt_coma
                '''
-    if isinstance(p[1], str):
-        p[0] = [p[1]]
+    if p[1][0] == "id":
+        p[0] = ("id_list", [p[1][1]])
     else:
-        p[0] = p[1] + [p[2]]
+        p[0] = ("id_list", p[1][1] + [p[2][1]])
 
 def p_ptr_vers(p):
     '''ptr_vers : POINTEUR
@@ -52,14 +49,18 @@ def p_ptr_vers(p):
 def p_opt_colon(p):
     ''' opt_colon : ':'
                   | empty'''
-    p[0] = p[1]
+    p[0] = ("opt_colon", p[1])
 
+
+def p_id(p):
+    '''id : ID'''
+    p[0] = ('id', p[1])
 
 # An optional coma
 def p_opt_coma(p):
     ''' opt_coma : ','
                   | empty'''
-    p[0] = p[1]
+    p[0] = ("opt_coma", p[1])
 
 def p_empty(p):
     '''empty : '''

490e166d406b17180373d9ba57cdc5e6c8ee183e
Gregory Holder <gregory.holder76@gmail.com>
2022-03-25 07:00:35 +0100

    Laptop merge

--- compiler.py
+++ compiler.py
@@ -1,4 +1,5 @@
 from parser import parser
+from lexer import lexer
 
 def compile(input_file, output_file):
     with open(input_file, 'r') as fp:
@@ -19,25 +20,67 @@ def try_compile(node):
     else:
         raise RuntimeError(f"Missing function for {type} node.")
 
-def c_var_defs(var_line_defs):
+def c_var_decls(var_line_decls):
     result = ""
-    for var_line_def in var_line_defs:
-        result += try_compile(var_line_def) + "\n"
+    for var_line_decl in var_line_decls:
+        result += try_compile(var_line_decl) + "\n"
 
     return result
     
 
-def c_var_line_def(id_list, type):
-    return f"{try_compile(type)} {try_compile(id_list)};"
+def add_inner_par(str):
+    return str.format("({})")
+
+def c_var_line_decl(id_list, type):
+
+    id_format = "{}"
+    last_modif = None
+
+    while type[0] != "type_id":
+        if type[0] == "table_type":
+            _, type, start, end = type
+            if last_modif == "ptr":
+                id_format = add_inner_par(id_format)
+
+            last_modif = "table"
+
+            size = int(end[1][1]) - int(start[1][1])
+            size = str(size)
+            
+            id_format = id_format.format(f"{{}}[{size}]")
+
+        elif type[0] == "ptr_type":
+            _, type = type
+            if last_modif == "table":
+                id_format = add_inner_par(id_format)
+
+            last_modif = "ptr"
+            id_format = id_format.format(f"*{{}}")
+
+        else:
+            raise Exception("")
+
+    ids_with_modifs = ", ".join([id_format.format(id) for id in id_list[1]])
+
+    return f"{type[1]} {ids_with_modifs};"
 
 def c_id_list(ids):
     return ", ".join(ids)
 
-def c_type(id):
-    return try_compile(id)
+def c_type(type):
+    return try_compile(type)
+
+def c_table_type(type, start, end):
+    return f"{try_compile(type)}[{try_compile(start)}]"
+
+def c_type_id(id):
+    return f"{id}"
 
 def c_ptr_type(type):
     return f"{try_compile(type)}*"
+
+def c_lit_num(lit_num):
+    return lit_num[1]
 
 def c_id(id):
     return f"{id}"
